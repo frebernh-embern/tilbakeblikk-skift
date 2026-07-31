2949ba3736b17df4143bdf4b14ed1606528b9610
frebernh-embern <frebernh@gmail.com>
2026-07-31 13:12:27 +0200

    Koble til Firebase (delt database)

--- firebase-config.js
+++ firebase-config.js
@@ -6,10 +6,11 @@
 //  og lagrer påmeldinger bare på din egen enhet.
 // ============================================================
 window.firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyBkN9E_Y_25jOFLUas1hSfNVfAV58VW_wA",
+  authDomain: "tilbakeblikk-skift.firebaseapp.com",
+  projectId: "tilbakeblikk-skift",
+  storageBucket: "tilbakeblikk-skift.firebasestorage.app",
+  messagingSenderId: "1095965205422",
+  appId: "1:1095965205422:web:989d316181ec6178d8de3f",
+  measurementId: "G-WP2P54T3WK"
 };
